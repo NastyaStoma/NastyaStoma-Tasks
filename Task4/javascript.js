79a04fc973ed973dc26d868149dc6aeea3078ae6
StomaAnastasia <Nastya_Stoma_Artist@mail.ru>
2018-11-05 01:11:50 +0300

    + beautifier

--- Task4/javascript.js
+++ Task4/javascript.js
@@ -1,5 +1,5 @@
 function primeNumbersToN(number) {
-	var ArrayOfPrimeNumbers=[];
+    var ArrayOfPrimeNumbers = [];
     if (number > 3) {
         for (var i = 2; i <= number; i++) {
             if (i % 2 !== 0 && i % i == 0 || i == 2) {
@@ -12,13 +12,13 @@ function primeNumbersToN(number) {
             ArrayOfPrimeNumbers.push(i);
         }
     }
-	return ArrayOfPrimeNumbers;
+    return ArrayOfPrimeNumbers;
 }
 
 function firstNprimeNumbers(amounth) {
-	var ArrayOfPrimeNumbers=[];
-	var FirstNprimeNumbersArray=[];
-	var number = 100000;
+    var ArrayOfPrimeNumbers = [];
+    var FirstNprimeNumbersArray = [];
+    var number = 100000;
     if (number > 3) {
         for (var i = 2; i <= number; i++) {
             if (i % 2 !== 0 && i % i == 0 || i == 2) {
@@ -31,41 +31,39 @@ function firstNprimeNumbers(amounth) {
             ArrayOfPrimeNumbers.push(i);
         }
     }
-	var StringOfPrime = ArrayOfPrimeNumbers.toString().split(",");
-	for (var i=0;i<amounth;i++)
-		{
-			FirstNprimeNumbersArray.push(StringOfPrime[i]);
-		}
-	return FirstNprimeNumbersArray;
+    var StringOfPrime = ArrayOfPrimeNumbers.toString().split(",");
+    for (var i = 0; i < amounth; i++) {
+        FirstNprimeNumbersArray.push(StringOfPrime[i]);
+    }
+    return FirstNprimeNumbersArray;
 }
 
 function SumOfFibonacci(limit) {
-	var number = 1000000;
-	var FirstRowValue = 0;
+    var number = 1000000;
+    var FirstRowValue = 0;
     var SecondRowValue = 1;
     var NextRowValue = 0;
-	var SumOfEvenValues = 0;
-	var FibonacciRow = [];
+    var SumOfEvenValues = 0;
+    var FibonacciRow = [];
     for (var i = 0; i < number; i++) {
         NextRowValue = FirstRowValue + SecondRowValue;
-        FirstRowValue = SecondRowValue; 
-        SecondRowValue = NextRowValue; 
-		FibonacciRow.push(NextRowValue);
+        FirstRowValue = SecondRowValue;
+        SecondRowValue = NextRowValue;
+        FibonacciRow.push(NextRowValue);
     }
-	for (i=0;i<limit;i++ ) 
-		{
-			if (FibonacciRow[i] % 2 == 0) {
-          SumOfEvenValues  += FibonacciRow[i];
-			}
-		}
-	return SumOfEvenValues;
-} 
+    for (i = 0; i < limit; i++) {
+        if (FibonacciRow[i] % 2 == 0) {
+            SumOfEvenValues += FibonacciRow[i];
+        }
+    }
+    return SumOfEvenValues;
+}
 
 function OrderOfNumbers(number) {
     var Order = [];
     var OrderSumma = 0;
-	var Result = [];
-	var TenLastNum = [];
+    var Result = [];
+    var TenLastNum = [];
     for (var i = 0; i <= number; i++) {
         Order[i] = Math.pow(i, i);
         OrderSumma += Order[i];
@@ -78,7 +76,7 @@ function OrderOfNumbers(number) {
     for (var i = 0; i <= 10; i++) {
         Result.push(SummaReversed[i]);
     }
-	return Result;
+    return Result;
 }
 
 function AverageOfArray(Array) {
@@ -86,15 +84,15 @@ function AverageOfArray(Array) {
     for (var i = 0; i < Array.length; i++) {
         Summa += Array[i];
     }
-	return (Summa / Array.length);
+    return (Summa / Array.length);
 }
 
 function ArrayMaxValue(Array) {
-	var MaxValue = 0;
+    var MaxValue = 0;
     for (var i = 0; i < Array.length; i++) {
         if (Array[i] > Array[0]) {
             Array[0] = Array[i];
-			MaxValue = Array[0];
+            MaxValue = Array[0];
         }
     }
     return MaxValue;
@@ -103,27 +101,26 @@ function ArrayMaxValue(Array) {
 function UniqueStrings(Array) {
     var UniqueStringsArray = [];
     for (var i = 0; i < Array.length; i++) {
-		if (!UniqueStringsArray.includes(Array[i]))
-			{
-				UniqueStringsArray.push(Array[i]);
-			}
-	}
+        if (!UniqueStringsArray.includes(Array[i])) {
+            UniqueStringsArray.push(Array[i]);
+        }
+    }
     return UniqueStringsArray;
 }
 
 function Palindrome(string) {
     var ReversedString = string.split("").reverse().join("");
-        if (string == ReversedString) {
-            return "Данная строка - полиндром";
-        } else {
-            return "Данная строка не является полиндромом";
-        }
+    if (string == ReversedString) {
+        return "Данная строка - полиндром";
+    } else {
+        return "Данная строка не является полиндромом";
+    }
 }
 
 function SummOfDigits(string) {
-     Summa = 0;
-    for (var i = 0; i < string.length; i++){
-		Summa += Number(string[i]);	
-	}
+    Summa = 0;
+    for (var i = 0; i < string.length; i++) {
+        Summa += Number(string[i]);
+    }
     return Summa;
 }
