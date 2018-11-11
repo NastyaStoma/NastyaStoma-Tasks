37bc4987614622d3e9fa09c56d49be8a6e6978ee
StomaAnastasia <Nastya_Stoma_Artist@mail.ru>
2018-11-11 19:47:44 +0300

    calendar done!

--- Task6/script.js
+++ Task6/script.js
@@ -22,7 +22,7 @@ previous.style.outline = 'none';
 previous.style.fontSize = '40px';
 var content = document.createElement('div');
 document.body.appendChild(content);
-content.className = "content"
+content.className = "content";
 content.style.display = 'flex';
 var CalendarPage = document.createElement('table');
 CalendarPage.className = 'CalendarPage';
@@ -54,103 +54,103 @@ var CurrDate = new Date();
 var CurrDay = CurrDate.getDate();
 var CurrMonth = CurrDate.getMonth();
 var CurrYear = CurrDate.getFullYear();
-function DaysNames()
-{
-	var NamesWrapp = document.createElement('div');
-	content.appendChild(NamesWrapp);
-	NamesWrapp.style.display = 'flex';
-	NamesWrapp.style.maxHeight = '30px';
-	NamesWrapp.style.position = 'absolute';
-	NamesWrapp.style.marginTop = '10px';
-	NamesWrapp.style.fontSize = '25px';
-	var mon = document.createElement('p');
-    NamesWrapp.appendChild(mon);
-	mon.innerHTML = "Mon";
-	mon.style.marginLeft ='10px';
-	mon.style.marginRight ='20px';
-	var tue = document.createElement('p');
-    NamesWrapp.appendChild(tue);
-	tue.innerHTML = "Tue ";
-	tue.style.marginRight ='22px';
-	var wed = document.createElement('p');
-    NamesWrapp.appendChild(wed);
-	wed.innerHTML = "Wed ";
-	wed.style.marginRight ='20px';
-	var thu = document.createElement('p');
-    NamesWrapp.appendChild(thu);
-	thu.innerHTML = "Thu ";
-	thu.style.marginRight ='30px';
-	var fri = document.createElement('p');
-    NamesWrapp.appendChild(fri);
-	fri.innerHTML = "Fri ";
-	fri.style.marginRight ='35px';
-	var sat = document.createElement('p');
-    NamesWrapp.appendChild(sat);
-	sat.innerHTML = "Sat ";
-	sat.style.marginRight ='30px';
-	var sun = document.createElement('p');
-    NamesWrapp.appendChild(sun);
-	sun.innerHTML = "Sun ";
-	sun.style.color = '#A52A2A';
+
+function DaysNames() {
+    var NamesWrapper = document.createElement('div');
+    content.appendChild(NamesWrapper);
+    NamesWrapper.style.display = 'flex';
+    NamesWrapper.style.maxHeight = '30px';
+    NamesWrapper.style.position = 'absolute';
+    NamesWrapper.style.marginTop = '10px';
+    NamesWrapper.style.fontSize = '25px';
+    var mon = document.createElement('p');
+    NamesWrapper.appendChild(mon);
+    mon.innerHTML = "Mon";
+    mon.style.marginLeft = '10px';
+    mon.style.marginRight = '20px';
+    var tue = document.createElement('p');
+    NamesWrapper.appendChild(tue);
+    tue.innerHTML = "Tue ";
+    tue.style.marginRight = '22px';
+    var wed = document.createElement('p');
+    NamesWrapper.appendChild(wed);
+    wed.innerHTML = "Wed ";
+    wed.style.marginRight = '20px';
+    var thu = document.createElement('p');
+    NamesWrapper.appendChild(thu);
+    thu.innerHTML = "Thu ";
+    thu.style.marginRight = '30px';
+    var fri = document.createElement('p');
+    NamesWrapper.appendChild(fri);
+    fri.innerHTML = "Fri ";
+    fri.style.marginRight = '35px';
+    var sat = document.createElement('p');
+    NamesWrapper.appendChild(sat);
+    sat.innerHTML = "Sat ";
+    sat.style.marginRight = '30px';
+    var sun = document.createElement('p');
+    NamesWrapper.appendChild(sun);
+    sun.innerHTML = "Sun ";
+    sun.style.color = '#A52A2A';
 }
 DaysNames();
-function table(TodayMonth) {
-    var p = document.createElement('p');
-    CalendarPage.appendChild(p);
-    p.innerHTML = CurrMonth;
-    //p.style.textTransform = 'uppercase';
-    p.style.width = '10px';
-    p.style.fontSize = '30px';
-    p.style.marginTop = '0px';
-	p.style.paddingLeft = '40px';
-	p.style.fontFamily = 'Permanent Marker';
+
+function DrawCalendar(TodayMonth) {
+    var MonthHeadline = document.createElement('p');
+    CalendarPage.appendChild(MonthHeadline);
+    MonthHeadline.innerHTML = CurrMonth;
+    MonthHeadline.style.width = '10px';
+    MonthHeadline.style.fontSize = '30px';
+    MonthHeadline.style.marginTop = '0px';
+    MonthHeadline.style.paddingLeft = '40px';
+    MonthHeadline.style.fontFamily = 'Permanent Marker';
     if (CurrMonth == 0) {
         var FirstTableDay = new Date(CurrYear, CurrMonth, 1);
-        p.innerHTML = 'January';
+        MonthHeadline.innerHTML = 'January';
         var weeks = 5;
     } else if (CurrMonth == 1) {
         var FirstTableDay = new Date(CurrYear, CurrMonth, -2);
-        p.innerHTML = 'February';
+        MonthHeadline.innerHTML = 'February';
         var weeks = 5;
     } else if (CurrMonth == 2) {
         var FirstTableDay = new Date(CurrYear, CurrMonth, -2);
-        p.innerHTML = 'March';
+        MonthHeadline.innerHTML = 'March';
         var weeks = 5;
     } else if (CurrMonth == 3) {
         var FirstTableDay = new Date(CurrYear, CurrMonth, -5);
-        p.innerHTML = 'April';
+        MonthHeadline.innerHTML = 'April';
         var weeks = 6;
     } else if (CurrMonth == 4) {
         var FirstTableDay = new Date(CurrYear, CurrMonth, 0);
-        p.innerHTML = 'May ';
+        MonthHeadline.innerHTML = 'May ';
         var weeks = 5;
     } else if (CurrMonth == 5) {
         var FirstTableDay = new Date(CurrYear, CurrMonth, -3);
-        p.innerHTML = 'June';
+        MonthHeadline.innerHTML = 'June';
         var weeks = 5;
     } else if (CurrMonth == 6) {
         var FirstTableDay = new Date(CurrYear, CurrMonth, -5);
-        p.innerHTML = 'July';
+        MonthHeadline.innerHTML = 'July';
         var weeks = 6;
     } else if (CurrMonth == 7) {
         var FirstTableDay = new Date(CurrYear, CurrMonth, -1);
-        p.innerHTML = 'August';
+        MonthHeadline.innerHTML = 'August';
         var weeks = 5;
     } else if (CurrMonth == 8) {
         var FirstTableDay = new Date(CurrYear, CurrMonth, -4);
-        p.innerHTML = 'September';
+        MonthHeadline.innerHTML = 'September';
         var weeks = 5;
     } else if (CurrMonth == 9) {
         var FirstTableDay = new Date(CurrYear, CurrMonth, 1);
-        p.innerHTML = 'October';
+        MonthHeadline.innerHTML = 'October';
         var weeks = 5;
     } else if (CurrMonth == 10) {
         var FirstTableDay = new Date(CurrYear, CurrMonth, -2);
-        p.innerHTML = 'November';
+        MonthHeadline.innerHTML = 'November';
         var weeks = 5;
     } else if (CurrMonth == 11) {
         var FirstTableDay = new Date(CurrYear, CurrMonth, -4);
-        p.innerHTML = 'December';
+        MonthHeadline.innerHTML = 'December';
         var weeks = 6;
     }
     for (var j = 0; j < weeks; j++) {
@@ -158,7 +158,7 @@ function table(TodayMonth) {
         for (var i = 0; i < 7; i++) {
             var td = document.createElement('td');
             td.style.width = '40px';
-			td.style.paddingLeft = '20px';
+            td.style.paddingLeft = '20px';
             td.innerHTML = FirstTableDay.getDate();
             FirstTableDay.setDate(FirstTableDay.getDate() + 1);
             if (td.innerHTML != CurrDay) {
@@ -166,60 +166,41 @@ function table(TodayMonth) {
             } else {
                 td.style.color = '#FFFFFF';
                 td.style.backgroundColor = '#A52A2A';
-				td.style.borderRadius = '5px';
+                td.style.borderRadius = '5px';
             }
-				if (td.innerHTML <= CurrDay) {
-                td.style.color = '#1E90FF';
-            }
-			var FirstMonthDay = new Date(CurrYear, CurrMonth, );
-			var LastMonthDay = new Date(CurrYear, CurrMonth, );
-			/*var CurrDate = new Date();
-               var CurrDay = CurrDate.getDate();
-               var CurrMonth = CurrDate.getMonth();
-               var CurrYear = CurrDate.getFullYear();*/
-			
-			
-			
-			
-			
-			
             tr.appendChild(td);
-            tr.style.backgroundColor = '#FFF0F5';
-			td.style.borderRadius = '5px';
+            tr.style.backgroundColor = '#DCDCDC';
+            td.style.borderRadius = '5px';
         }
         CalendarPage.appendChild(tr);
     }
-	 var p2 = document.createElement('p');
-    CalendarPage.appendChild(p2);
-    p2.innerHTML = '2018';
-	p2.style.marginTop = '0px';
 }
-table(CurrMonth);
+DrawCalendar(CurrMonth);
+
 function previousMonth() {
-	if(CurrMonth >0){
-    CalendarPage.innerHTML = '';
-    CurrMonth = CurrMonth - 1;
-    table(CurrMonth);
-	}
-	else {
-		CalendarPage.innerHTML = '';
-		CurrMonth = 12;
-    	CurrMonth = CurrMonth - 1;
-    	table(CurrMonth);
-	}
+    if (CurrMonth > 0) {
+        CalendarPage.innerHTML = '';
+        CurrMonth = CurrMonth - 1;
+        DrawCalendar(CurrMonth);
+    } else {
+        CalendarPage.innerHTML = '';
+        CurrMonth = 12;
+        CurrMonth = CurrMonth - 1;
+        DrawCalendar(CurrMonth);
+    }
 }
+
 function nextMonth() {
-	if(CurrMonth <11){
-    CalendarPage.innerHTML = '';
-    CurrMonth = CurrMonth + 1;
-    table(CurrMonth);
-	}
-	else {
-		CalendarPage.innerHTML = '';
-		CurrMonth = -1; 
-    	CurrMonth = CurrMonth + 1;
-    	table(CurrMonth);
-	}
+    if (CurrMonth < 11) {
+        CalendarPage.innerHTML = '';
+        CurrMonth = CurrMonth + 1;
+        DrawCalendar(CurrMonth);
+    } else {
+        CalendarPage.innerHTML = '';
+        CurrMonth = -1;
+        CurrMonth = CurrMonth + 1;
+        DrawCalendar(CurrMonth);
+    }
 }
 document.getElementById('Prev').onclick = previousMonth;
 document.getElementById('Nex').onclick = nextMonth;
