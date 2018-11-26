7184f7b400d0b0d2bf52ae35682918175c4f9c07
StomaAnastasia <Nastya_Stoma_Artist@mail.ru>
2018-11-26 21:33:36 +0300

    Task 10: almost done

---
+++ ask10/webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+module.exports = {
+	entry: './src/index.js',
+	output: {
+		path: path.join(__dirname, 'build'),
+		filename: 'bundle.js',
+		publicPath: '/build/'
+	},
+	module: {
+		rules: [
+			{
+				test: /\.js/,
+			}
+		]
+	}
+}
